cab3aa44df5023cb56501787c31233c94ce44053
thenewkgb <74326914+thenewkgb@users.noreply.github.com>
2022-08-13 07:32:54 +0100

    added for loop for more variations

--- main.cpp
+++ main.cpp
@@ -1,58 +1,89 @@
+//
+// SDL graphical - infinite line
+//
+//
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <vector>
+#include <random>
 
 void addright(std::vector<int> &vtr)
 {
-    int middle = vtr.size() / 2;
-    int largest = vtr[middle];
-    // difference between two
-    int ans = vtr[middle] - vtr[middle - 1];
+    // which numbers do we want?
+    int first = vtr.size() / 2;
+    int second = first-1;
+    //difference between the two
+    int ans = vtr[first] - vtr[second];
 
     int i = vtr.size() - vtr.size() / 2;
+    
     for (int j = 1; j < i; ++j)
-        vtr[middle + j] = largest + ans * j;
+    {
+        vtr[first + j] = ans + vtr[second + j];
+        ans = vtr[first+j] - vtr[second + j];
+    }
     return;
 }
 
 void addleft(std::vector<int> &vtr)
 {
-    int middle = vtr.size() / 2;
-    int first = vtr[middle - 1];
-    int second = vtr[middle];
-    int ans = first - second;
+    int first = vtr.size() / 2;
+    int second = first-1;
+    int ans = vtr[second] - vtr[first];
 
     int i = vtr.size() - vtr.size() / 2;
     for (int j = 1; j < i; ++j)
     {
-        vtr[middle - j - 1] = first + ans * j;
+        vtr[second-j] = ans + vtr[first-j];
+        ans = vtr[second-j] - vtr[first-j];
     }
     return;
 }
 
 void adddown(std::vector<std::vector<int>> &arr, int column)
 {
-    int verticlemiddle = arr.size() / 2;
-    int first = arr[verticlemiddle][column];
-    int second = arr[verticlemiddle - 1][column];
-    int ans = first - second;
-
-    int i = arr.size() - arr.size() / 2;
-    for (int j = 1; j < i; ++j)
-        arr[verticlemiddle + j][column] = first + ans * j;
-    return;
+    int first = arr.size()/2;
+    int second = first -1;
+    int ans = arr[first][column] - arr[second][column];
+        
+    int i = arr.size() - arr.size()/2;
+    for(int j=1;j<i;++j)
+    {
+        arr[first + j][column] =
+             ans + arr[second+j][column];
+        ans = arr[first+j][column] - 
+            arr[second+j][column];
+    }
+        return;
 }
 
 void addup(std::vector<std::vector<int>> &arr, int column)
 {
-    int verticlemiddle = arr.size() / 2;
-    int first = arr[verticlemiddle][column];
-    int second = arr[verticlemiddle - 1][column];
-    int ans = second - first;
+    int first = arr.size()/2;
+    int second = first - 1;
+    int ans = arr[second][column] - arr[first][column];
+    
+    int i = arr.size() - arr.size()/2;
+    for(int j=1;j<i;++j)
+    {
+        arr[second - j][column] =
+            ans + arr[first-j][column];
+        ans = arr[second-j][column] - arr[first-j][column];
+    }
+    return;
+}
 
-    int i = arr.size() - arr.size() / 2;
-    for (int j = 1; j < i; ++j)
-        arr[verticlemiddle - j - 1][column] = second + ans * j;
+void changeRedSurfaceColour(SDL_Surface *surface, int val)
+{
+    int newValue = (val*8)%200;
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, newValue, 0, 0));
+}
+
+void changeGreenSurfaceColour(SDL_Surface *surface, int val)
+{
+    int newValue = (val*8)%255;
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, newValue, 0));
 }
 
 void printline(SDL_Surface* redsurface, SDL_Surface* greensurface, SDL_Surface* bluesurface, SDL_Surface* surface, int row, std::vector<int> value)
@@ -70,9 +101,16 @@ void printline(SDL_Surface* redsurface, SDL_Surface* greensurface, SDL_Surface* 
         position.x = position.x * i;
         
         if(value[i-1] < 0)
+        {
+            changeRedSurfaceColour(redsurface, value[i-1]);
+            
             SDL_BlitSurface(redsurface, NULL, surface, &position);
+        }
         else if(value[i-1] > 0)
+        {
+            changeGreenSurfaceColour(greensurface, value[i-1]);
             SDL_BlitSurface(greensurface, NULL, surface, &position);
+        }
         else if(value[i-1] == 0)
             SDL_BlitSurface(bluesurface, NULL, surface, &position);
         
@@ -125,29 +163,6 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // my code
-    
-    // change these values to produce
-    // different patterns
-    
-    int one{7};
-    int two{3};
-    int three{21};
-
-    std::vector<std::vector<int>> arr =
-        {{0, 0, 0, 0, 0, 0, 0, 0},
-         {0, 0, 0, 0, 0, 0, 0, 0},
-         {0, 0, 0, 0, 0, 0, 0, 0},
-         {0, 0, 0, 0, one, 0, 0, 0},
-         {0, 0, 0, two, three, 0, 0, 0},
-         {0, 0, 0, 0, 0, 0, 0, 0},
-         {0, 0, 0, 0, 0, 0, 0, 0},
-         {0, 0, 0, 0, 0, 0, 0, 0}};
-
-    initgrid(arr);
-    
-    // SDL set up
-
     SDL_Window *window = NULL;
     window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
@@ -157,13 +172,12 @@ int main(int argc, char *argv[])
         return 1;
     }
     
+    // surface to display on window
+    
        SDL_Surface *surface = NULL;
     surface = SDL_GetWindowSurface(window);
     
-     // draw to off screen surface
-    // blit surface to screen
-    // update window surface
-    // see results
+     // create three surfaces to draw off screen
 
     SDL_Surface *redsurface = NULL;
     redsurface = SDL_CreateRGBSurface(0, 50, 50, 32, 0, 0, 0, 0);
@@ -174,23 +188,75 @@ int main(int argc, char *argv[])
     SDL_Surface *bluesurface = NULL;
     bluesurface = SDL_CreateRGBSurface(0, 50, 50, 32, 0, 0, 0, 0);
     
-    // fill surfaces with red, green and blue
+    // fill these surfaces with red, green and blue
 
-    SDL_FillRect(redsurface, NULL, SDL_MapRGB(redsurface->format, 0xFF, 0x00, 0x00));
+    SDL_FillRect(redsurface, NULL, SDL_MapRGB(redsurface->format, 255, 0, 0));
     
      SDL_FillRect(greensurface, NULL, SDL_MapRGB(greensurface->format, 0x00, 0xFF, 0x00));
     
     SDL_FillRect(bluesurface, NULL, SDL_MapRGB(bluesurface->format, 0x00, 0x00, 0xFF));
     
+     // my code
+    
+    // change these values to produce
+    // different patterns
+    
+    int w {0};
+    int x {1};
+    int y {2};
+    int z {3};
+
+    std::vector<std::vector<int>> arr =
+        {{0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, w, x, 0, 0, 0},
+         {0, 0, 0, y, z, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0}};
+
+    initgrid(arr);
+    
     // get them on screen
     
     printgrid(arr, redsurface, greensurface, bluesurface, surface);
-    
-    
-
 
     SDL_UpdateWindowSurface(window);
     SDL_Delay(5000);
+    
+    srand(time(0));
+    
+    for(int i = 0; i<10;++i)
+    {
+    
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
+    
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(1000);
+    
+    w = rand() % 20;
+    x = rand() % 20;
+    y = rand() % 20;
+    z = rand() % 20;
+    
+    arr =
+        {{0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, w, x, 0, 0, 0},
+         {0, 0, 0, y, z, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0}};
+    
+    initgrid(arr);
+    printgrid(arr, redsurface, greensurface, bluesurface, surface);
+    
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(5000);
+    }
+    
     
     // clean up
 
